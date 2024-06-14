3e85c690813c000b6697ffaad8e33405d82be3b2
mhmdiamd <m1pro2021@mhmdiamd.local>
2024-06-14 08:43:32 +0700

    init

--- app/src/main/java/com/plcoding/meditationuiyoutube/ui/HomeScreen.kt
+++ app/src/main/java/com/plcoding/meditationuiyoutube/ui/HomeScreen.kt
@@ -1,10 +1,22 @@
 package com.plcoding.meditationuiyoutube.ui
 
+import android.graphics.Paint.Align
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.GridCells
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.LazyVerticalGrid
@@ -14,10 +26,10 @@ import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -26,25 +38,32 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.plcoding.meditationuiyoutube.BottomMenuContent
-import com.plcoding.meditationuiyoutube.Feature
+import com.plcoding.meditationuiyoutube.ui.theme.DeepBlue
 import com.plcoding.meditationuiyoutube.R
-import com.plcoding.meditationuiyoutube.standardQuadFromTo
+import com.plcoding.meditationuiyoutube.data.BottomMenuContent
+import com.plcoding.meditationuiyoutube.data.Feature
+import com.plcoding.meditationuiyoutube.ui.theme.BlueViolet1
+import com.plcoding.meditationuiyoutube.ui.theme.ButtonBlue
+import com.plcoding.meditationuiyoutube.ui.theme.DarkerButtonBlue
+import com.plcoding.meditationuiyoutube.ui.theme.LightRed
+import com.plcoding.meditationuiyoutube.ui.theme.TextWhite
+import com.plcoding.meditationuiyoutube.utils.standardQuadFromTo
 import com.plcoding.meditationuiyoutube.ui.theme.*
 
-@ExperimentalFoundationApi
+@Preview(showBackground = true, name = "Home Screen")
 @Composable
 fun HomeScreen() {
     Box(
-        modifier = Modifier
-            .background(DeepBlue)
-            .fillMaxSize()
-    ) {
+       modifier = Modifier
+           .background(DeepBlue)
+           .fillMaxSize()
+    ){
         Column {
-            GreetingSection()
-            ChipSection(chips = listOf("Sweet sleep", "Insomnia", "Depression"))
+            GreetingSection(name = "Ilham")
+            ChipSection(chips = listOf("Sweet sleep", "Insomnia", "Depression", "Stress", "Mental Ines"))
             CurrentMeditation()
             FeatureSection(
                 features = listOf(
@@ -79,28 +98,33 @@ fun HomeScreen() {
                 )
             )
         }
-        BottomMenu(items = listOf(
-            BottomMenuContent("Home", R.drawable.ic_home),
-            BottomMenuContent("Meditate", R.drawable.ic_bubble),
-            BottomMenuContent("Sleep", R.drawable.ic_moon),
-            BottomMenuContent("Music", R.drawable.ic_music),
-            BottomMenuContent("Profile", R.drawable.ic_profile),
-        ), modifier = Modifier.align(Alignment.BottomCenter))
+        BottomMenu(
+            modifier = Modifier.align(Alignment.BottomCenter),
+            items = listOf(
+                BottomMenuContent("Home", R.drawable.ic_home),
+                BottomMenuContent("Meditate", R.drawable.ic_bubble),
+                BottomMenuContent("Sleep", R.drawable.ic_moon),
+                BottomMenuContent("Mu   sic", R.drawable.ic_music),
+                BottomMenuContent("Profile", R.drawable.ic_profile),
+            )
+        )
     }
 }
 
 @Composable
 fun BottomMenu(
-    items: List<BottomMenuContent>,
+    items : List<BottomMenuContent>,
     modifier: Modifier = Modifier,
-    activeHighlightColor: Color = ButtonBlue,
-    activeTextColor: Color = Color.White,
-    inactiveTextColor: Color = AquaBlue,
-    initialSelectedItemIndex: Int = 0
+    activeHighlightColor : Color = ButtonBlue,
+    activeTextColor : Color = Color.White,
+    inactiveTextColor : Color = AquaBlue,
+    initialSelectedItemIndex: Int = 0,
 ) {
+
     var selectedItemIndex by remember {
         mutableStateOf(initialSelectedItemIndex)
     }
+
     Row(
         horizontalArrangement = Arrangement.SpaceAround,
         verticalAlignment = Alignment.CenterVertically,
@@ -109,13 +133,14 @@ fun BottomMenu(
             .background(DeepBlue)
             .padding(15.dp)
     ) {
-        items.forEachIndexed { index, item ->
+        items.forEachIndexed {
+            index, item ->
             BottomMenuItem(
                 item = item,
                 isSelected = index == selectedItemIndex,
                 activeHighlightColor = activeHighlightColor,
                 activeTextColor = activeTextColor,
-                inactiveTextColor = inactiveTextColor
+                inactiveTextColor = inactiveTextColor,
             ) {
                 selectedItemIndex = index
             }
@@ -125,31 +150,34 @@ fun BottomMenu(
 
 @Composable
 fun BottomMenuItem(
-    item: BottomMenuContent,
+    item : BottomMenuContent,
     isSelected: Boolean = false,
     activeHighlightColor: Color = ButtonBlue,
     activeTextColor: Color = Color.White,
     inactiveTextColor: Color = AquaBlue,
-    onItemClick: () -> Unit
+    onItemClick : () -> Unit
 ) {
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
-        modifier = Modifier.clickable {
-            onItemClick()
-        }
+        modifier = Modifier
+            .clickable {
+                onItemClick()
+            }
     ) {
         Box(
             contentAlignment = Alignment.Center,
             modifier = Modifier
                 .clip(RoundedCornerShape(10.dp))
-                .background(if (isSelected) activeHighlightColor else Color.Transparent)
+                .background(
+                    if (isSelected) activeHighlightColor else Color.Transparent
+                )
                 .padding(10.dp)
         ) {
             Icon(
                 painter = painterResource(id = item.iconId),
                 contentDescription = item.title,
-                tint = if (isSelected) activeTextColor else inactiveTextColor,
+                tint = if(isSelected) activeTextColor else inactiveTextColor,
                 modifier = Modifier.size(20.dp)
             )
         }
@@ -158,24 +186,28 @@ fun BottomMenuItem(
             color = if(isSelected) activeTextColor else inactiveTextColor
         )
     }
+
 }
 
 @Composable
 fun GreetingSection(
-    name: String = "Philipp"
+    modifier: Modifier = Modifier,
+    name : String
 ) {
+
     Row(
-        horizontalArrangement = Arrangement.SpaceBetween,
-        verticalAlignment = Alignment.CenterVertically,
         modifier = Modifier
             .fillMaxWidth()
-            .padding(15.dp)
+            .padding(16.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
     ) {
         Column(
+            modifier = Modifier,
             verticalArrangement = Arrangement.Center
         ) {
             Text(
-                text = "Good morning, $name",
+                text = "Good Morning $name",
                 style = MaterialTheme.typography.h2
             )
             Text(
@@ -183,6 +215,7 @@ fun GreetingSection(
                 style = MaterialTheme.typography.body1
             )
         }
+
         Icon(
             painter = painterResource(id = R.drawable.ic_search),
             contentDescription = "Search",
@@ -194,27 +227,35 @@ fun GreetingSection(
 
 @Composable
 fun ChipSection(
-    chips: List<String>
+    modifier: Modifier = Modifier,
+    chips : List<String>
 ) {
+
     var selectedChipIndex by remember {
         mutableStateOf(0)
     }
-    LazyRow {
-        items(chips.size) {
+
+    LazyRow(
+
+    ) {
+        items(chips.size){
             Box(
                 contentAlignment = Alignment.Center,
                 modifier = Modifier
-                    .padding(start = 15.dp, top = 15.dp, bottom = 15.dp)
+                    .padding(
+                        start = 15.dp,
+                        top = 15.dp,
+                        bottom = 15.dp
+                    )
                     .clickable {
                         selectedChipIndex = it
                     }
                     .clip(RoundedCornerShape(10.dp))
                     .background(
-                        if (selectedChipIndex == it) ButtonBlue
-                        else DarkerButtonBlue
+                        if (selectedChipIndex == it) ButtonBlue else DarkerButtonBlue
                     )
                     .padding(15.dp)
-            ) {
+            ){
                 Text(text = chips[it], color = TextWhite)
             }
         }
@@ -223,7 +264,7 @@ fun ChipSection(
 
 @Composable
 fun CurrentMeditation(
-    color: Color = LightRed
+    color : Color = LightRed
 ) {
     Row(
         verticalAlignment = Alignment.CenterVertically,
@@ -241,7 +282,7 @@ fun CurrentMeditation(
                 style = MaterialTheme.typography.h2
             )
             Text(
-                text = "Meditation • 3-10 min",
+                text = "Meditation - 3 - 10 min",
                 style = MaterialTheme.typography.body1,
                 color = TextWhite
             )
@@ -258,28 +299,34 @@ fun CurrentMeditation(
                 painter = painterResource(id = R.drawable.ic_play),
                 contentDescription = "Play",
                 tint = Color.White,
-                modifier = Modifier.size(16.dp)
+                modifier = Modifier
+                    .size(16.dp)
             )
         }
     }
 }
 
-@ExperimentalFoundationApi
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun FeatureSection(features: List<Feature>) {
-    Column(modifier = Modifier.fillMaxWidth()) {
+fun FeatureSection(
+    features : List<Feature>
+) {
+    Column {
         Text(
             text = "Features",
             style = MaterialTheme.typography.h1,
             modifier = Modifier.padding(15.dp)
         )
+        
         LazyVerticalGrid(
             cells = GridCells.Fixed(2),
             contentPadding = PaddingValues(start = 7.5.dp, end = 7.5.dp, bottom = 100.dp),
-            modifier = Modifier.fillMaxHeight()
+            modifier = Modifier
+                .fillMaxHeight()
         ) {
-            items(features.size) {
-                FeatureItem(feature = features[it])
+            items(features.size){
+                index ->
+                FeatureItem(feature = features[index])
             }
         }
     }
@@ -287,7 +334,7 @@ fun FeatureSection(features: List<Feature>) {
 
 @Composable
 fun FeatureItem(
-    feature: Feature
+    feature : Feature
 ) {
     BoxWithConstraints(
         modifier = Modifier
@@ -299,12 +346,12 @@ fun FeatureItem(
         val width = constraints.maxWidth
         val height = constraints.maxHeight
 
-        // Medium colored path
-        val mediumColoredPoint1 = Offset(0f, height * 0.3f)
-        val mediumColoredPoint2 = Offset(width * 0.1f, height * 0.35f)
-        val mediumColoredPoint3 = Offset(width * 0.4f, height * 0.05f)
-        val mediumColoredPoint4 = Offset(width * 0.75f, height * 0.7f)
-        val mediumColoredPoint5 = Offset(width * 1.4f, -height.toFloat())
+        // Medium color path
+        val mediumColoredPoint1 = Offset(x = 0f, y = height * 0.3f)
+        val mediumColoredPoint2 = Offset(x = width * 0.1f, y = height * 0.35f)
+        val mediumColoredPoint3 = Offset(x = width * 0.4f, y = height * 0.05f)
+        val mediumColoredPoint4 = Offset(x = width * 0.75f, y = height * 0.7f)
+        val mediumColoredPoint5 = Offset(x = width * 1.4f, y = height * 0.7f)
 
         val mediumColoredPath = Path().apply {
             moveTo(mediumColoredPoint1.x, mediumColoredPoint1.y)
@@ -312,7 +359,7 @@ fun FeatureItem(
             standardQuadFromTo(mediumColoredPoint2, mediumColoredPoint3)
             standardQuadFromTo(mediumColoredPoint3, mediumColoredPoint4)
             standardQuadFromTo(mediumColoredPoint4, mediumColoredPoint5)
-            lineTo(width.toFloat() + 100f, height.toFloat() + 100f)
+            lineTo(width.toFloat(), height.toFloat() + 100f)
             lineTo(-100f, height.toFloat() + 100f)
             close()
         }
@@ -334,9 +381,9 @@ fun FeatureItem(
             lineTo(-100f, height.toFloat() + 100f)
             close()
         }
+
         Canvas(
-            modifier = Modifier
-                .fillMaxSize()
+            modifier = Modifier.fillMaxSize()
         ) {
             drawPath(
                 path = mediumColoredPath,
@@ -355,29 +402,38 @@ fun FeatureItem(
             Text(
                 text = feature.title,
                 style = MaterialTheme.typography.h2,
-                lineHeight = 26.sp,
+                lineHeight = 20.sp,
                 modifier = Modifier.align(Alignment.TopStart)
             )
             Icon(
                 painter = painterResource(id = feature.iconId),
-                contentDescription = feature.title,
+                contentDescription = "Icon - $feature.title",
                 tint = Color.White,
                 modifier = Modifier.align(Alignment.BottomStart)
             )
             Text(
                 text = "Start",
                 color = TextWhite,
-                fontSize = 14.sp,
                 fontWeight = FontWeight.Bold,
+                fontSize = 14.sp,
                 modifier = Modifier
-                    .clickable {
-                        // Handle the click
-                    }
                     .align(Alignment.BottomEnd)
                     .clip(RoundedCornerShape(10.dp))
                     .background(ButtonBlue)
                     .padding(vertical = 6.dp, horizontal = 15.dp)
+                    .clickable {
+
+                    }
             )
         }
     }
 }
+
+
+
+
+
+
+
+
+
